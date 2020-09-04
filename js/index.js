5a68b64a79ee0a94ed4306d125e82f58b8312bd3
Rahul <rahul.shardiya@webcraft.co.in>
2020-09-04 09:08:53 +0530

    completed

---
+++ js/index.js
@@ -0,0 +1,47 @@
+
+  	function validateForm() {
+  var name =  document.getElementById('name').value;
+  if (name == "") {
+      document.querySelector('.status').innerHTML = "Name cannot be empty";
+      return false;
+  }
+  var email =  document.getElementById('email').value;
+  if (email == "") {
+      document.querySelector('.status').innerHTML = "Email cannot be empty";
+      return false;
+  } else {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if(!re.test(email)){
+          document.querySelector('.status').innerHTML = "Email format invalid";
+          return false;
+      }
+  }
+  var subject =  document.getElementById('subject').value;
+  if (subject == "") {
+      document.querySelector('.status').innerHTML = "Subject cannot be empty";
+      return false;
+  }
+  var message =  document.getElementById('message').value;
+  if (message == "") {
+      document.querySelector('.status').innerHTML = "Message cannot be empty";
+      return false;
+  }
+  document.querySelector('.status').innerHTML = "Sending...";
+  sendEmail();
+}
+
+
+
+function sendEmail() {
+	Email.send({
+	Host: "smtp.gmail.com",
+	Username : "temprorymail1@gmail.com",
+	Password : "tempmailpass",
+	To : 'kuldeepsalve786@gmail.com',
+	From : document.getElementById('email').value,
+	Subject : document.getElementById('subject').value,
+	Body : "Name: "+document.getElementById('name').value +'<br>'+"Email: "+document.getElementById('email').value+"<br>"+"Message: "+document.getElementById('message').value,
+	}).then(
+		message =>  document.querySelector('.status').innerHTML = "Message Sent Successfully."
+	);
+}
